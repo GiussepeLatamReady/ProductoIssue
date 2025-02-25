a92679a8fc2c27a747765ad7d2dc9a0cb3aaf84c
Giussepe Delgado <giussepe@latamready.com>
2025-02-25 16:01:38 -0500

    IBPT
    Proceso de creacion de script

--- SMC_GADP_MPRD_V2.1.js
+++ SMC_GADP_MPRD_V2.1.js
@@ -54,6 +54,7 @@ define([
                     //log.error("transaction map",transaction)
                     //transaction = addItems(transaction);
                     //log.error("transaction",transaction)
+                    setTransactionFields(transaction)
                     context.write({
                         key: context.key,
                         value: transaction
@@ -92,6 +93,77 @@ define([
             }
 
         }
+
+        const getTransaction = () => {
+            const transactions =[];
+            let columns = [];
+            columns.push("internalid");
+            columns.push(search.createColumn({ name: 'formulatext', formula: '{tranid}', sort: search.Sort.DESC }));
+            
+            search.create({
+                type: search.Type.INVOICE,
+                filters: [
+                    ['mainline', 'is', 'T'],  // Solo linea principal
+                    'AND',
+                    ['subsidiary', 'anyof', "60"]],
+                columns: columns
+            }).run().each(result => {
+                const {columns,getValue} = result;
+                transactions.push({
+                    internalid: getValue(columns[0]),
+                    tranid: getValue(columns[1])
+                })
+                return true;
+            });
+
+            return transactions
+        }
+
+        const setTransactionFields = (transaction) => {
+            search.create({
+                type: "customrecord_lmry_br_transaction_fields",
+                filters: [
+                    ["custrecord_lmry_br_related_transaction","anyof",transaction.id],
+                    "AND",
+                    ['isinactive', 'is', 'F'],  // Solo linea principal
+                    'AND',
+                    ['subsidiary', 'anyof', "60"]],
+                columns: [
+                    "custrecord_lmry_br_trib_fed_nac",
+                    "custrecord_lmry_br_trib_fed_imp",
+                    "custrecord_lmry_br_trib_est",
+                    "custrecord_lmry_br_trib_mun"
+                ]
+            }).run().each(result => {
+                const {columns,getValue} = result;
+                transaction.nacional = getValue(columns[0]);
+                transaction.importacion = getValue(columns[1]);
+                transaction.estadual = getValue(columns[2]);
+                transaction.municipal = getValue(columns[3]);
+            })
+        }
+
+        const generateFile = (transactions) => {
+
+            const lines = items.map(item => [
+                item.period, // 1. Periodo.
+                item.catalogueCode, // 2. Código del catálogo utilizado.
+                item.typeOfExistence, // 3. Tipo de existencia.
+                item.codeOfExistence, // 4. Código propio de la existencia.
+                item.catalogueCode, // 5. Repetido: Código del catálogo utilizado.
+                item.correspondingStockCode, // 6. Código de Existencia correspondiente.
+                item.existenceDescription, // 7. Descripción de la existencia.
+                item.unitOfMeasureCode, // 8. Código de la Unidad de medida.
+                item.valuationMethodCode, // 9. Código del método de valuación.
+                item.quantity, // 10. Cantidad de la existencia.
+                item.unitCost, // 11. Costo unitario de la existencia.
+                item.totalCost, // 12. Costo total.
+                "1", // 13. Indica el estado de la operación. Siempre "1" en este caso.
+                "" // 14. Campos de libre utilización.
+            ].join(",") + '\n');
+            
+            return lines.join('');
+        };
 
         return { getInputData, map, summarize };
     });
